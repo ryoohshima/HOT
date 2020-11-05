470a90850e310037ae0dff2658e0c48bac581f72
OhshimaRyo <ryo.ohshima@actdesign-lab.co.jp>
2020-11-05 18:35:20 +0900

    update

--- js/index.js
+++ js/index.js
@@ -39,24 +39,37 @@ icon.on("click", () => {
 // ***********
 // * company *
 // ***********
+let val;
+const change_contents = (val) => {
+    // コンテンツ全非表示
+    $("#company_contents div").fadeOut();
+    // タブに連動するコンテンツを表示
+    setTimeout(function() {
+        $("#" + TAB_ARRAY[val]).fadeIn();
+    }, 500);
+};
 
-// コンテンツ切り替え
+// コンテンツ切り替え(pc)
 const TAB_ARRAY = {
     "company_tab_philosophy": "company_contents_philosophy",
     "company_tab_about":      "company_contents_about",
     "company_tab_history":    "company_contents_history",
     "company_tab_overview":   "company_contents_overview",
 };
-$("#company_tab_wrap div").click(function(){
+
+$("#company_tab_wrap div").click(function() {
     // class全削除
     $("#company_tab_wrap div").removeClass("active_tab");
     // クリックしたタブのみクラス追加
     $(this).addClass("active_tab");
 
     // クリックしたタブのid取得
-    const click_tab = $(this).attr("id")
-    // コンテンツ全非表示
-    $("#company_contents div").hide();
-    // タブに連動するコンテンツを表示
-    $("#" + TAB_ARRAY[click_tab]).show();
+    val = $(this).attr("id")
+    change_contents(val);
+});
+
+// コンテンツ切り替え(sp)
+$("#company_pulldown_wrap select").change(function() {
+    val = $(this).val();
+    change_contents(val);
 });
